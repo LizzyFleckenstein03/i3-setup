ab56d0711471b7042d5a6c31a16d62bb2b0a2730
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-11-22 10:15:27 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+sudo add-apt-repository -y ppa:regolith-linux/stable
+sudo apt install -y i3-gaps feh picom batcat lolcat cowsay zsh lua git
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+mkdir -p ~/bin/
+cp cat ~/bin/
+
+mkdir -p ~/.config/i3/
+cp i3-config ~/.config/i3/config
+
+mkdir -p ~/.config/kitty/
+cp kitty.conf ~/.config/kitty/kitty.conf
+
+mkdir -p ~/.config/
+cp picom.conf ~/.config/picom.conf
+
+mkdir -p ~/.oh-my-zsh/themes/
+cp fleckenstein.zsh-theme ~/.oh-my-zsh/themes/fleckenstein.zsh-theme
+
+mkdir -p ~/
+cp zshrc ~/.zshrc
---
+++ zshrc
@@ -0,0 +1,7 @@
+export ZSH="$HOME/.oh-my-zsh"
+export PATH="$HOME/bin:$PATH"
+ZSH_THEME="fleckenstein"
+USER_ALIAS="ふれけんしたいん"
+USER_ALIAS_LENGTH=16
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
